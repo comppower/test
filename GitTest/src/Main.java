7abb360f8c28dffd04063ca4c9660d1ab9966a0a
Kent L <comppower15@gmail.com>
2016-03-11 22:06:05 -0500

    Initial checkin

---
+++ GitTest/src/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("This is a");
+		temp test = new temp();
+		System.out.println(test.a);
+
+
+	}
+
+}
---
+++ GitTest/src/temp.java
@@ -0,0 +1,10 @@
+
+public class temp {
+	public int a;
+	
+	temp()
+	{
+		a=0;
+	}
+
+}
